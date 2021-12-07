5821b2035b0feaa3d15ab965b9dc76484e3ecdf4
Isabelle05 <78001703+Isabelle05@users.noreply.github.com>
2021-12-06 20:45:07 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
@@ -18,6 +22,7 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot() {
@@ -29,7 +34,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 13;
+        noseY = results[0].pose.nose.y + 5 ;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
